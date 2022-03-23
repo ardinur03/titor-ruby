0c0e256dafcb6ee5d7a8df4b9cb3ca16f75ba5b5
Luthfie Yannuardy <luthfie.yannuardy.tif21@polban.ac.id>
2022-03-23 15:41:27 +0700

    FIX: mengubah parameter procedure & fungsi pada kelolaText

--- kelolaText.h
+++ kelolaText.h
@@ -10,17 +10,17 @@
 #define CTRL_V 22
 
 void printText(int jmlBaris, char text[MAXBARIS][MAXKOLOM]);
-void commandMode(int *jmlBaris, char (*text)[MAXBARIS][MAXKOLOM]);
-int insertMode(int jmlBaris, char (*text)[MAXBARIS][MAXKOLOM]);
-void editBaris(int curBaris, char (*text)[MAXBARIS][MAXKOLOM]);
+void commandMode(int *jmlBaris, char text[MAXBARIS][MAXKOLOM]);
+int insertMode(int jmlBaris, char text[MAXBARIS][MAXKOLOM]);
+void editBaris(int curBaris, char text[MAXBARIS][MAXKOLOM]);
 void printLabelCmdMode();
 void incCurBaris(int *curBaris, int jmlBaris);
 void decCurBaris(int *curBaris, int jmlBaris);
-void delBaris(int curBaris, int *jmlBaris, char (*text)[MAXBARIS][MAXKOLOM]);
-void addNewBaris(int curBaris, int *jmlBaris, char (*text)[MAXBARIS][MAXKOLOM]);
+void delBaris(int curBaris, int *jmlBaris, char text[MAXBARIS][MAXKOLOM]);
+void addNewBaris(int curBaris, int *jmlBaris, char text[MAXBARIS][MAXKOLOM]);
 bool barisPenuh(int jmlBaris);
 void copyBaris(int curBaris, char *temp, bool *isClipboardEmpty, char text[MAXBARIS][MAXKOLOM]);
-void pasteBaris(int curBaris, int *jmlBaris, char *temp, bool isClipboardEmpty, char (*text)[MAXBARIS][MAXKOLOM]);
+void pasteBaris(int curBaris, int *jmlBaris, char *temp, bool isClipboardEmpty, char text[MAXBARIS][MAXKOLOM]);
 // void saveFile(int jmlBaris, char text[MAXBARIS][MAXKOLOM]);
 
 #endif
