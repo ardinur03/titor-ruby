b337810465316c21822f9afc790b6162e799c9d2
Muhamad Ardi Nur Insan <muhamad.ardi.tif21@polban.ac.id>
2022-05-22 12:50:09 +0700

    FIX : delete files that are not in use

--- loading.h
+++
@@ -1,8 +0,0 @@
-#ifndef loading_H
-#define loading_H
-
-void gotoxy(int X, int y); //fungsi yang digunakan untuk memindahkan cursor (garis vertikal atau horizontal yang berkedip-kedip)
-						  // di layar ke posisi yang diinginkan
-void loading_screen(); //tampilan loading
-
-#endif
