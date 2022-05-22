f3feb41d493d3685b642a19320f00aa62902c3f5
Muhamad Ardi Nur Insan <muhamad.ardi.tif21@polban.ac.id>
2022-05-22 22:26:04 +0700

    FEAT : change menu pull down in app.cpp

--- header.h
+++ header.h
@@ -1,6 +1,8 @@
 #ifndef header_h
 #define header_h
 
+#include <windows.h>
+
 /**
  * @include {modul} main.cpp
 */
@@ -8,7 +10,7 @@ void app(); //  modul menu utama home
 void loading_screen(); // modul tampilan 
 
 /**
- * @include {modul} app.cpp
+ * @include {modul} app.cpp  Start
 */
 void home(); // modul menu utama home
 void backMenuHome(); // modul kembali ke modul home()
@@ -18,10 +20,13 @@ void procedureShorcut(); // prosedur yang berisi shorcut aplikasi TiTOR
 void newFile(); // modul untuk memanggil newFile dan berisi manageText 
 void deleteFile();
 /** deleteFile ini digunakan untuk menghapus file yang ada di dalam direktori secara dinamis berdasarkan nama file yang di inputkan pengguna */
+
 void duplicateFile();
 /**  duplicateFile ini digunakan untuk menduplikasi file yang ada di dalam direktori secara dinamis */
+
 void findAndReplaceFile();
 /** modul findAndReplaceFile digunakan untuk mencari kata dan mengganti kata dalam file */
+
 void findReplace(char *str, const char *oldWord, const char *newWord);
 /** modul findAndReplaceFile digunakan untuk mencari kata lama dan mengganti kata baru dalam file 
  * @param str 
@@ -30,8 +35,14 @@ void findReplace(char *str, const char *oldWord, const char *newWord);
 */
 void openFileTwoDimensi();
 /** open file menjadi array 2 dimensi dengan kolom dan baris terdapat validasi kosong di penginputan nama file hitung baris, text kemdian di tampilkan ke layar menggunakan modul insertMode */
+
 void printFromFile(char path[50]);
 /** printFromFile di gunakan untuk menampilkan isi file digunakan di find & replace (include findReplace.cpp & openFile.cpp)*/
+
 void renameFile();
 /** renameFile ini digunakan untuk mengganti nama file yang ada di dalam direktori secara dinamis */
+
+/**
+ * @include {modul} app.cpp  End
+*/
 #endif
